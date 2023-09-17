60bf6a5e5f911d6ebfc59301804b459b9f463993
Jacob <jacobc@bu.edu>
2023-09-17 01:11:51 -0400

    VoiceJit.py

--- Backend/VoiceJit.py
+++ Backend/VoiceJit.py
@@ -1,19 +1,23 @@
 import openai
 import speech_recognition as sr
 import pyttsx3
-from flask import Flask, request, jsonify
+import os
+import json
 
-key = 'API KEY'
+key = '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'
 
 openai.api_key = key
 
 personality = "personality.txt"
+
 with open(personality, "r") as file:
     mode = file.read()
 
 messages = [{"role": "system", "content": "What is your name?"}]
 
 engine = pyttsx3.init()
+# engine = pyttsx3.init(driverName='nss')
+
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[2].id)
 
@@ -22,29 +26,107 @@ mic = sr.Microphone()
 r.dynamic_energy_threshold = False
 r.energy_threshold = 400
 
-app = Flask(__name__)
 
-@app.route('/ask', methods=['POST'])
-def ask():
-    global messages  # Needed to modify the global copy of messages
+# BRUH This whisper shit cost money
+# while True:
+#     with mic as source:
+#         print("\nListening...")
+#         r.adjust_for_ambient_noise(source, duration = 0.5)
+#         audio = r.listen(source)
+#         try:
+#             with open('speech.wav', 'wb') as f:
+#                 f.write(audio.get_wav_data())
+#             speech = open('speech.wav', 'rb')
+#             wcompletion = openai.Audio.transcribe(
+#                 model = "whisper-1",
+#                 file = speech
+#             )
+#             user_input = wcompletion['text']
+#             print(user_input)
+#         except:
+#             continue
 
-    user_input = request.json['user_input']
 
-    messages.append({"role": "user", "content": user_input})
+# def save_conversation(save_foldername):
+#     '''
+#     Checks the folder for previous conversations and will get the next suffix that has not been used yet.  
+#     It returns suffix number
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301",
-        messages=messages,
-        temperature=0.8
-    )
-
-    response = completion.choices[0].message.content
-    messages.append({"role": "assistant", "content": response})
-
-    engine.say(f'{response}')
-    # engine.runAndWait()  # Comment this out if you don't want to hear the response
+#     Args:
+#         save_foldername (str) : Takes in the path to save the conversation to.
+#     '''
     
-    return jsonify({"response": response})
+#     os.makedirs(save_foldername, exist_ok=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#     base_filename = 'conversation'
+#     suffix = 0
+#     filename = os.path.join(save_foldername, f'{base_filename}_{suffix}.txt')
+
+#     while os.path.exists(filename):
+#         suffix += 1
+#         filename = os.path.join(save_foldername, f'{base_filename}_{suffix}.txt')
+
+#     with open(filename, 'w') as file:
+#         json.dump(messages, file, indent=4)
+
+#     return suffix
+
+# def save_inprogress(suffix, save_foldername):
+#     '''
+#     Uses the suffix number returned from save_conversation to continually update the 
+#     file for this instance of execution.  This is so that you can save the conversation 
+#     as you go so if it crashes, you don't lose to conversation.  Shouldn't be called
+#     from outside of the class.
+
+#     Args:
+#         suffix  :  Takes suffix count from save_conversation()
+#     '''
+#     os.makedirs(save_foldername, exist_ok=True)
+#     base_filename = 'conversation'
+#     filename = os.path.join(save_foldername, f'{base_filename}_{suffix}.txt')
+
+#     with open(filename, 'w') as file:
+#         json.dump(messages, file, indent=4)
+
+
+
+# # grab script location
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# foldername = "voice_assistant"
+# save_foldername = os.path.join(script_dir,f"conversations/{foldername}")
+# # suffix = save_conversation(save_foldername)
+
+
+while True:
+    try:
+        with mic as source:
+            print("\nListening...")
+            r.adjust_for_ambient_noise(source, duration = 0.5)
+            audio = r.listen(source)
+            try:
+                user_input = r.recognize_google(audio)
+                if user_input == 'quit':
+                    break
+                print(user_input)
+            except:
+                continue
+
+        messages.append({"role" : "user", "content" : user_input})
+
+
+        completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0301",
+                messages=messages,
+                temperature=0.8
+            )
+
+        response = completion.choices[0].message.content
+        messages.append({"role": "assistant", "content": response})
+        print(f"\n{response}\n")
+        # save_inprogress(suffix, save_foldername)
+
+        engine.say(f'{response}')
+        # engine.runAndWait()
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        break  # This will exit the loop if an error occurs. You can remove this if you want the loop to continue.
--- src/index.js
+++ src/index.js
@@ -1,96 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const axios = require('axios');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 const FLASK_SERVER_URL = 'http://127.0.0.1:5000/ask';
 
+// Middleware to parse POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Node.js server is running!');
-});
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.post('/ask', async (req, res) => {
+// Endpoint to get a response from the Flask server
+app.post('/get-response', async (req, res) => {
     try {
-        const userInput = req.body.user_input;
-
-        // Make a request to Flask server
-        const response = await axios.post(FLASK_SERVER_URL, {
-            user_input: userInput
-        });
-
+        const user_input = req.body.user_input;
+        const response = await axios.post(FLASK_SERVER_URL, { user_input });
         res.json(response.data);
     } catch (error) {
-        console.error('Error contacting Flask server:', error.message);
-        res.status(500).send('Error contacting Flask server.');
+        res.status(500).send("Error connecting to the Flask server");
     }
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import axios from 'axios';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // import React from 'react';
-// // import ReactDOM from 'react-dom';
-// // import './index.css';
-// // import App from './App';
-// // import axios from 'axios';
-
-// const RootComponent = () => {
-//   const [response, setResponse] = React.useState("");
-//   const [userInput, setUserInput] = React.useState("");
-
-//   const requestMicrophonePermission = async () => {
-//     try {
-//       // Request microphone access
-//       await navigator.mediaDevices.getUserMedia({ audio: true });
-//       alert('Microphone access granted!');
-//     } catch (error) {
-//       if (error.name === 'NotAllowedError') {
-//         alert('Permission to access microphone was denied.');
-//       } else {
-//         alert('An error occurred: ' + error.message);
-//       }
-//     }
-//   };
-
-//   const handleListenClick = async () => {
-//     try {
-//       const result = await axios.post('http://localhost:5000/listen');
-//       setUserInput(result.data.userInput);
-//       setResponse(result.data.response);
-//     } catch (error) {
-//       console.error("Error:", error);
-//     }
-//   };
-
-//   return (
-//     <React.StrictMode>
-//       <App />
-//       <button onClick={requestMicrophonePermission}>Request Microphone Access</button>
-//       <button onClick={handleListenClick}>Start Listening</button>
-//       <textarea 
-//         value={userInput} 
-//         readOnly 
-//         placeholder="What the backend is listening to..."
-//         rows="4" 
-//         cols="50"
-//       />
-//       <p>{response}</p>
-//     </React.StrictMode>
-//   );
-// };
-
-// ReactDOM.render(<RootComponent />, document.getElementById('root'));
